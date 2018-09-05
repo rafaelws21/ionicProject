27268e1293df8c2794fc30cbfd39588cadab7266
rafael.willians <rafael.sousa@ctis.com.br>
2018-09-05 14:26:42 -0300

    implementação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  tarefas: Array<{nome: string, descricao:string, acao:any}>;  
+
   constructor(public navCtrl: NavController) {
+
+    this.tarefas = [
+      {
+        nome:'Nome da tarefa', 
+        descricao:'Descrição da tarefa', 
+        acao:[
+          {label:'Editar', icone:'create'},
+          {label: 'Visualizar', icone:'eye'}
+        ]
+      },
+      {
+        nome:'Nome da tarefa2', 
+        descricao:'Descrição da tarefa2', 
+        acao:[
+          {label:'Editar', icone:'create'},
+          {label: 'Visualizar', icone:'eye'}
+        ]
+      }
+    ]
 
   }
 
